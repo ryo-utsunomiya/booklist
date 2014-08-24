0a753e29e837fea1ed80c8d0e3e8c0929d9f80da
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2014-08-24 16:00:22 +0900

    Add building process of Phalcon to provision.sh

--- provision.sh
+++ provision.sh
@@ -4,13 +4,17 @@
 #sudo dpkg-reconfigure locales
 
 # install packages
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install apache2 php5 php5-mysql php5-dev libpcre3-dev gcc make
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y apache2 php5 php5-mysql php5-dev libpcre3-dev gcc make vim tree
 
 # install phalcon
-mkdir ~/src
-cd ~/src
+cd /tmp
 git clone --depth=1 git://github.com/phalcon/cphalcon.git
 cd cphalcon/build
 sudo ./install
+
+sudo cp /var/www/booklist/server/etc/apache2/sites-available/default /etc/apache2/sites-available/default
+sudo cp /var/www/booklist/server/etc/php5/conf.d/30-phalcon.ini /etc/php5/conf.d/30-phalcon.ini
+
+sudo service apache2 restart
