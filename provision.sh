0ddabf4df2fc1c3fd8035d7eaeee222fa7c22237
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2014-08-24 16:38:52 +0900

    Add bashrc

--- provision.sh
+++ provision.sh
@@ -6,7 +6,12 @@
 # install packages
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y apache2 php5 php5-mysql php5-dev libpcre3-dev gcc make vim tree
+sudo apt-get install -y apache2 php5 php5-mysql php5-dev libpcre3-dev gcc make
+sudo apt-get install -y vim tree
+
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root-password password root'
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root-password_admin password root'
+#sudo apt-get install -y mysql-server
 
 # install phalcon
 cd /tmp
@@ -16,5 +21,6 @@ sudo ./install
 
 sudo cp /var/www/booklist/server/etc/apache2/sites-available/default /etc/apache2/sites-available/default
 sudo cp /var/www/booklist/server/etc/php5/conf.d/30-phalcon.ini /etc/php5/conf.d/30-phalcon.ini
+sudo cp /var/www/booklist/server/home/vagrant/.bashrc /home/vagrant/.bashrc
 
 sudo service apache2 restart
